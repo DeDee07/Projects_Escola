0a4934a62edf54dbaef2e5f17793e28d8d320c39
Dede07 <154218049+DeDee07@users.noreply.github.com>
2025-04-08 13:34:29 +0100

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -20,6 +20,29 @@ def exercicio_2():
     elif numero_secreto == palpite:
         print("Wow acertaste com um total de {tentativa} tentativa(s)")  
     
+def exercicio_3():
+    nu1 = int(input("Escolha um número: "))
+    nu2 = int(input("Escolhe outro número: "))
+    for i in range(nu1, nu2 + 1 ):
+        print(i, end=" ")
+
+def exercicio_4():
+    nu1 = float(input("Escolha um número: "))
+    nu2 = float(input("Escolha outro número: "))
+    
+    soma = nu1 + nu2
+    subtracao = nu1 - nu2
+    multiplicacao = nu1 * nu2
+    
+    if nu2 == 0:
+        divisao = "não é possível dividir por zero"
+    else:
+        divisao = nu1 / nu2
+
+    print("A Soma é:", soma)
+    print("A subtração é:", subtracao)
+    print("A multiplicação é:", multiplicacao)
+    print("A Divisão é:", divisao)
 
 def menu():
     while True:
@@ -28,18 +51,22 @@ def menu():
         print("\n" + "="*30)
         print("1. Soma de dois números")
         print("2. Adivinha o número")
+        print("3. Contagem de x até y")
         print("0. Sair do Exercício")
+        print("\n" + "="*30)
 
-        escolha = input("Escolha uma opção de (0-2)")
+        escolha = input("Escolha uma opção de (0-7): ")
 
         if escolha == '1':
             exercicio_1()
         elif escolha == '2':
             exercicio_2()
+        elif escolha == '3':
+            exercicio_3()
         elif escolha == '0':
             print("tchauzinho...")
             break
         else:
-            print("Opção inválida, tente um número (0-2)")
+            print("Opção inválida, tente um número (0-7)")
 
 menu()
